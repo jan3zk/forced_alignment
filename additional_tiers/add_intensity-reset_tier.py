1392f7b91d5c53dd301a359d07cf62b2f913fad2
jan3zk <janez.krizzaj@gmail.com>
2023-11-21 11:44:26 +0100

    Updated intensity-reset and speech-rate-reduction computation

--- additional_tiers/add_intensity-reset_tier.py
+++ additional_tiers/add_intensity-reset_tier.py
@@ -1,17 +1,17 @@
 import sys
 import parselmouth
-from utils import parse_textgrid, write_textgrid  
+import numpy as np
+from utils import parse_textgrid, write_textgrid
 
-
-def detect_intensity_resets(audio_path, input_textgrid, output_textgrid, intensity_reset_threshold=5):
+def detect_intensity_resets(audio_path, input_textgrid, output_textgrid, intensity_reset_threshold=7, method="near"):
     # Load the audio and the TextGrid
     sound = parselmouth.Sound(audio_path)
     tiers = parse_textgrid(input_textgrid)
     
     # Analyze intensity
     intensity = sound.to_intensity()
-    intensity_values = intensity.values.flatten()
-    
+    intensity_values = intensity.values[0]  # Assuming one channel
+
     # Get the syllable tier
     syllable_tier = tiers['cnvrstl-syllables']
     
@@ -19,30 +19,47 @@ def detect_intensity_resets(audio_path, input_textgrid, output_textgrid, intensi
     intensity_reset_tier = []
     num_intervals = len(syllable_tier)
     
-    previous_syllable_mean_intensity = None
-    
     for i in range(num_intervals):
         # Get the start and end time of the syllable
         start_time, end_time, _ = syllable_tier[i]
         
         # Extract intensity values for the current syllable
-        start_index = int(start_time // intensity.dt)
-        end_index = int(end_time // intensity.dt)
+        start_index = int(start_time // intensity.time_step)
+        end_index = int(end_time // intensity.time_step)
         syllable_intensity_values = intensity_values[start_index:end_index]
+        syllable_intensity_values = syllable_intensity_values[syllable_intensity_values > 50]  # Include only valus greater than 50 dB
         syllable_mean_intensity = syllable_intensity_values.mean() if len(syllable_intensity_values) > 0 else 0
-        
+
+        if method == "near":
+            # Calculate mean intensity of the nearest neighbors
+            neighbors_mean_intensity = []
+            for j in range(max(0, i-1), min(i+2, num_intervals)):
+                if j != i:
+                    neighbor_start_time, neighbor_end_time, _ = syllable_tier[j]
+                    neighbor_intensity_values = intensity_values[int(neighbor_start_time // intensity.time_step):int(neighbor_end_time // intensity.time_step)]
+                    neighbor_intensity_values = neighbor_intensity_values[neighbor_intensity_values > 50]  # Include only valus greater than 50 dB
+                    neighbors_mean_intensity.append(neighbor_intensity_values.mean() if len(neighbor_intensity_values) > 0 else 0)
+
+            reference_mean_intensity = np.mean(neighbors_mean_intensity) if neighbors_mean_intensity else syllable_mean_intensity
+
+        elif method == "extended":
+            # Calculate mean intensity of the nearest four neighbors
+            neighbors_mean_intensity = []
+            for j in range(max(0, i-2), min(i+3, num_intervals)):
+                if j != i:
+                    neighbor_start_time, neighbor_end_time, _ = syllable_tier[j]
+                    neighbor_intensity_values = intensity_values[int(neighbor_start_time // intensity.time_step):int(neighbor_end_time // intensity.time_step)]
+                    neighbor_intensity_values = neighbor_intensity_values[neighbor_intensity_values > 50]  # Include only valus greater than 50 dB
+                    neighbors_mean_intensity.append(neighbor_intensity_values.mean() if len(neighbor_intensity_values) > 0 else 0)
+
+            reference_mean_intensity = np.mean(neighbors_mean_intensity) if neighbors_mean_intensity else syllable_mean_intensity
+
         # Check for intensity reset
-        intensity_reset_occurs = (
-            previous_syllable_mean_intensity is not None and
-            syllable_mean_intensity - previous_syllable_mean_intensity >= intensity_reset_threshold
-        )
-        
+        intensity_reset_occurs = abs(syllable_mean_intensity - reference_mean_intensity) >= intensity_reset_threshold
+
         # Label the interval
         label = "POS" if intensity_reset_occurs else "NEG"
         intensity_reset_tier.append((start_time, end_time, label))
-        
-        # Update the previous syllable mean intensity
-        previous_syllable_mean_intensity = syllable_mean_intensity
     
     # Add the new tier to the TextGrid
     tiers['intensity-reset'] = intensity_reset_tier
@@ -51,8 +68,7 @@ def detect_intensity_resets(audio_path, input_textgrid, output_textgrid, intensi
     write_textgrid(output_textgrid, tiers)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: python add_intensity-reset_tier.py [input.wav] [input.TextGrid] [output.TextGrid] [intensity_reset_threshold]")
+    if len(sys.argv) != 6:
+        print("Usage: python add_intensity-reset_tier.py [input.wav] [input.TextGrid] [output.TextGrid] [intensity_reset_threshold] [method]")
     else:
-        detect_intensity_resets(sys.argv[1], sys.argv[2], sys.argv[3], float(sys.argv[4]))
-
+        detect_intensity_resets(sys.argv[1], sys.argv[2], sys.argv[3], int(sys.argv[4]), sys.argv[5])
--- additional_tiers/add_speech-rate-reduction_tier.py
+++ additional_tiers/add_speech-rate-reduction_tier.py
@@ -2,7 +2,7 @@ import sys
 import parselmouth
 from utils import parse_textgrid, write_textgrid  
 
-def detect_speech_rate_reduction(audio_path, input_textgrid, output_textgrid, reduction_threshold=0.5):
+def detect_speech_rate_reduction(audio_path, input_textgrid, output_textgrid, reduction_threshold, method="near"):
     # Load the audio and the TextGrid
     sound = parselmouth.Sound(audio_path)
     tiers = parse_textgrid(input_textgrid)
@@ -12,35 +12,32 @@ def detect_speech_rate_reduction(audio_path, input_textgrid, output_textgrid, re
 
     # Create a new tier for speech rate reduction
     speech_rate_reduction_tier = []
-    previous_end_time = 0
-    previous_rate = 0
 
     for i, (start_time, end_time, _) in enumerate(syllable_tier):
-        # Calculate the speech rate as syllables per second
-        if i == 0:
-            previous_end_time = end_time
-            continue  # Skip the first syllable as we need a pair to compare the rate
+        # Calculate the length of the current syllable
+        syllable_length = end_time - start_time
         
-        # Calculate duration between the end of the previous syllable and the end of the current syllable
-        duration = end_time - previous_end_time
-        # Check for zero or negative duration to avoid division by zero or incorrect values
-        if duration <= 0:
-            continue
+        neighbors = []
+        if method == "near":
+            range_indices = range(max(0, i-1), min(len(syllable_tier), i+2))
+        elif method == "extended":
+            range_indices = range(max(0, i-2), min(len(syllable_tier), i+3))
 
-        # Calculate the current speech rate
-        current_rate = 1 / duration
+        for j in range_indices:
+            if j != i:
+                neighbor_start_time, neighbor_end_time, _ = syllable_tier[j]
+                neighbor_length = neighbor_end_time - neighbor_start_time
+                neighbors.append(neighbor_length)
         
-        # Check for speech rate reduction (we invert the threshold as we're looking for a decrease)
-        rate_reduction_occurs = (previous_rate - current_rate) >= reduction_threshold
-        
+        average_neighbor_length = sum(neighbors) / len(neighbors) if neighbors else 0
+
+        # Check for speech rate reduction
+        rate_reduction_occurs = syllable_length > (average_neighbor_length * reduction_threshold)
+
         # Label the interval
         label = "POS" if rate_reduction_occurs else "NEG"
         speech_rate_reduction_tier.append((start_time, end_time, label))
-        
-        # Update the previous values
-        previous_end_time = end_time
-        previous_rate = current_rate
-    
+
     # Add the new tier to the TextGrid
     tiers['speech-rate-reduction'] = speech_rate_reduction_tier
     
@@ -48,7 +45,7 @@ def detect_speech_rate_reduction(audio_path, input_textgrid, output_textgrid, re
     write_textgrid(output_textgrid, tiers)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: python add_speech-rate-reduction_tier.py [input.wav] [input.TextGrid] [output.TextGrid] [reduction_threshold]")
+    if len(sys.argv) != 6:
+        print("Usage: python add_speech-rate-reduction_tier.py [input.wav] [input.TextGrid] [output.TextGrid] [reduction_threshold] [method]")
     else:
-        detect_speech_rate_reduction(sys.argv[1], sys.argv[2], sys.argv[3], float(sys.argv[4]))
+        detect_speech_rate_reduction(sys.argv[1], sys.argv[2], sys.argv[3], float(sys.argv[4]), sys.argv[5])
