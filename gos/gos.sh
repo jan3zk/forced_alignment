224cbca875b032018d2b4062d19b86e8047718b3
jan3zk <janez.krizzaj@gmail.com>
2023-12-03 19:04:11 +0100

    Updated scripts for running on GOS database.

--- gos/gos.sh
+++ gos/gos.sh
@@ -55,33 +55,3 @@ for wav_file in $gos_dir/Artur-WAV/$folder*.wav; do
     fi
     counter=$((counter + 1))  # Increment the counter by 1
 done
-
-# Add tiers
-#for wav_file in $gos_dir/Artur-WAV/$folder*.wav; do
-#    wav_file=$gos_dir/Artur-WAV/Artur-J-Gvecg-P500014-avd.wav
-#    base_name=$(basename "$wav_file")
-#    xml_file=../data/Gos.TEI.2.1/Artur-J/${base_name/-avd.wav/.xml}
-#    textgrid_file=$out_dir/$folder/TextGrid/${base_name/.wav/.TextGrid}
-#    textgrid_file_out=$out_dir/$folder/TextGrid_final/${base_name/.wav/.TextGrid}
-#    
-#    echo ""
-#    echo wav_file=\"$wav_file\"
-#    echo xml_file=\"$xml_file\"
-#    echo textgrid_file=\"$textgrid_file\"
-#    echo textgrid_file_out=\"$textgrid_file_out\"
-#    
-#    # Add new tiers
-#    python ../add_cnvrstl-syllables_tier.py $textgrid_file $xml_file $textgrid_file_out
-#    python ../add_speaker-ID_tier.py $textgrid_file_out $xml_file $textgrid_file_out
-#    python ../add_standardized-trs_tier.py $xml_file $textgrid_file_out $textgrid_file_out
-#    python ../add_conversational-trs_tier.py $xml_file $textgrid_file_out $textgrid_file_out
-#    python ../add_cnvrstl-wrd-sgmnt_tier.py $xml_file $textgrid_file_out $textgrid_file_out
-#    python ../add_discourse-marker_tier.py $textgrid_file_out $textgrid_file_out ../data/discourse_markers.txt
-#    python ../add_pitch-reset_tier.py $wav_file $textgrid_file_out $textgrid_file_out 40 average-neighboring    
-#    python ../add_intensity-reset_tier.py $wav_file $textgrid_file_out $textgrid_file_out 8 near
-#    python ../add_speech-rate-reduction_tier.py $wav_file $textgrid_file_out $textgrid_file_out 1.5 near
-#    python ../add_pause_tier.py $textgrid_file_out $textgrid_file_out
-#    python ../add_speaker-change_tier.py $textgrid_file_out $textgrid_file_out
-#    python ../add_word-ID_tier.py $textgrid_file_out $xml_file $textgrid_file_out
-#    break
-#done
