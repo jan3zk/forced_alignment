6048f185eed49cf40364bbfae13d6545310e21ca
jan3zk <janez.krizzaj@gmail.com>
2024-02-02 13:38:21 +0100

    easily switch between fragmentation of inputs for MFA and using whole length files

--- fragmentize_trs_wav.py
+++ fragmentize_trs_wav.py
@@ -2,6 +2,8 @@ import os
 import sys
 import subprocess
 from utils_tei import intervals_from_tei
+from utils_tei import text_from_tei
+import shutil
 
 def combine_intervals(intervals, duration):
     """
@@ -37,44 +39,57 @@ def combine_intervals(intervals, duration):
     return merged_intervals
 
 def fragmentize_trs_wav(xml_path, wav_path, out_dir, duration):
-    trs_intervals = intervals_from_tei(xml_path, True)
-    trs_combined = combine_intervals(trs_intervals, duration)
 
     # Extract the base name of the trs file without extension
     base_name = os.path.splitext(os.path.basename(xml_path))[0]
 
-    # Ensure output directory exists
-    if not os.path.exists(out_dir):
-        os.makedirs(out_dir)
+    if duration != float('inf'):
+        trs_intervals = intervals_from_tei(xml_path, True)
+        trs_combined = combine_intervals(trs_intervals, duration)
 
-    # Create txt and corresponding wav files for each combined interval
-    for idx, (tmin, tmax, text) in enumerate(trs_combined):
-        formatted_tmin = f"{tmin:08.3f}"
-        formatted_tmax = f"{tmax:08.3f}"
+        # Ensure output directory exists
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
 
-        # Construct file names
-        txt_file_name = f"{base_name}_{formatted_tmin}_{formatted_tmax}_{idx:03d}.txt"
-        wav_file_name = f"{base_name}_{formatted_tmin}_{formatted_tmax}_{idx:03d}.wav"
-        txt_file_path = os.path.join(out_dir, txt_file_name)
-        wav_file_path = os.path.join(out_dir, wav_file_name)
-        
-        # Replace '-' for mfa to treat words connected by '-' as separate words
-        #text = text.replace('-', ' ')
-        # Remove all occurrences of "()"
-        text = text.replace("()", "")
-        # Remove all occurrences of "+"
-        text = text.replace("+", "")
+        # Create txt and corresponding wav files for each combined interval
+        for idx, (tmin, tmax, text) in enumerate(trs_combined):
+            formatted_tmin = f"{tmin:08.3f}"
+            formatted_tmax = f"{tmax:08.3f}"
 
-        # Write text file
+            # Construct file names
+            txt_file_name = f"{base_name}_{formatted_tmin}_{formatted_tmax}_{idx:03d}.txt"
+            wav_file_name = f"{base_name}_{formatted_tmin}_{formatted_tmax}_{idx:03d}.wav"
+            txt_file_path = os.path.join(out_dir, txt_file_name)
+            wav_file_path = os.path.join(out_dir, wav_file_name)
+
+            # Replace '-' for mfa to treat words connected by '-' as separate words
+            #text = text.replace('-', ' ')
+            # Remove all occurrences of "()"
+            text = text.replace("()", "")
+            # Remove all occurrences of "+"
+            text = text.replace("+", "")
+
+            # Write text file
+            with open(txt_file_path, 'w') as file:
+                file.write(text)
+
+            # Trim the wav file using ffmpeg
+            duration = tmax - tmin
+            subprocess.call(['ffmpeg', '-i', wav_path, '-ss', str(tmin), '-t', str(duration), wav_file_path])
+    else:
+        # Read entire text regardelss of time intervals
+        text = text_from_tei(xml_path, True)
+
+        # Write text to file
+        txt_file_path = os.path.join(out_dir, base_name+'.txt')
         with open(txt_file_path, 'w') as file:
             file.write(text)
 
-        # Trim the wav file using ffmpeg
-        duration = tmax - tmin
-        subprocess.call(['ffmpeg', '-i', wav_path, '-ss', str(tmin), '-t', str(duration), wav_file_path])
+        # Copy wav
+        shutil.copy(wav_path, os.path.join(out_dir, base_name+'.wav'))
 
 if __name__ == '__main__':
-    # This script fragments a TEI-formatted transcription (.trs) file along with its corresponding audio file (.wav) into smaller segments.
+    # This script fragments a TEI-formatted transcription (.xml) file along with its corresponding audio file (.wav) into smaller segments.
     # It takes four arguments:
     #   xml_path: The path to the TEI-formatted transcription file.
     #   wav_path: The path to the audio file associated with the transcription.
